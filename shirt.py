6f72f23aaa6c3b1a7ca460cd6be257f28c87f368
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-02 22:54:42 -0600

    Create variable for supported image file types, split command line arguments and get variable for file extensions

--- shirt.py
+++ shirt.py
@@ -13,5 +13,9 @@ def check_arg(argv):
         elif len(argv) > 3:
             exit('Too many command-line arguments')
 
+        support = ['.jpeg', '.jpg', '.png']
+        input_file_root, input_file_ext = splitext(argv[1])
+        output_file_root, output_file_ext = splitext(argv[2])
+
 if __name__ == '__main__':
     main()
