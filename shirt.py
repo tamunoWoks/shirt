44aeddc6fcfbdf04868abff20b59e6fe1519ae04
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-02 22:49:36 -0600

    Create func to validate command-line arguments, check that argv == 3

--- shirt.py
+++ shirt.py
@@ -7,7 +7,11 @@ def main():
 
 
 def check_arg(argv):
-    
+    try:
+        if len(argv) < 3:
+            exit('Too few command-line arguments')
+        elif len(argv) > 3:
+            exit('Too many command-line arguments')
 
 if __name__ == '__main__':
     main()
